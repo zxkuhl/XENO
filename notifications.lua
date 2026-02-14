a7c3791afdc77076a6a12fc7176775a82198da0d
zxkuhl <zero98122@gmail.com>
2026-02-13 22:33:58 -0600

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -4,5 +4,4 @@ return {
     {title = "HI", message = "hello - zxkuhl (note: this nodifications thingy is new!)"},
     {title = "DISCORD", message = "Join our community server for support"},
     {title = "PATCH", message = "Fixed bugs and improved performance"},
-
 }
