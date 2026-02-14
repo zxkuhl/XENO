750f75eb816aeee21d9e88a379732d7612ee4fff
zxkuhl <zero98122@gmail.com>
2026-02-14 10:43:54 -0600

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -5,4 +5,6 @@ return {
     {title = "DISCORD", message = "Join our community server for support"},
     {title = "PATCH", message = "Fixed bugs and improved performance"},
     {title = "VALINTINES", message = "no update yet but happy valintines day!"},
+    {title = "UP & COMING", message = "FORSAKEN SCRIPT! // COMING SOON"},
+    {title = "UP & COMING", message = "ABYSS SCRIPT! // COMING SOON"},
 }
