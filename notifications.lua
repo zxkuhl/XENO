bf62f96454afefbbc4360c66d9d143ad69cf7323
zxkuhl <zero98122@gmail.com>
2026-02-13 22:29:38 -0600

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -4,8 +4,5 @@ return {
     {title = "HI", message = "hello - zxkuhl (note: this nodifications thingy is new!)"},
     {title = "DISCORD", message = "Join our community server for support"},
     {title = "PATCH", message = "Fixed bugs and improved performance"},
-    {title = "WELCOME", message = "Thanks for using XENO!"},
-    {title = "HI", message = "hello - zxkuhl (note: this nodifications thingy is new!)"},
-    {title = "DISCORD", message = "Join our community server for support"},
-    {title = "PATCH", message = "Fixed bugs and improved performance"},
+
 }
