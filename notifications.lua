c23371337a92d1c7ad97b8e1012c91e18a2cb458
zxkuhl <zero98122@gmail.com>
2026-02-14 18:38:08 -0600

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -3,8 +3,8 @@ return {
     {title = "WELCOME", message = "Thanks for using XENO!"},
     {title = "HI", message = "hello - zxkuhl (note: this nodifications thingy is new!)"},
     {title = "DISCORD", message = "Join our community server for support"},
-    {title = "PATCH", message = "Fixed bugs and improved performance"},
     {title = "VALINTINES", message = "no update yet but happy valintines day!"},
     {title = "UP & COMING", message = "FORSAKEN SCRIPT! // COMING SOON"},
     {title = "NEW RELEASE", message = "ABYSS SCRIPT! // NEW, TRY IT OUT!"},
+    {title = "SCRIPT UPDATE", message = "ABYSS SCRIPT UPDATE /// added flight thingy and tried to fix auto farm"},
 }
