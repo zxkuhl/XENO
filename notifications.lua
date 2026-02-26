ee85541cbe1ade08bef71f12f38bcba4b45a171f
zxkuhl <zero98122@gmail.com>
2026-02-26 07:56:35 -0600

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -3,7 +3,6 @@ return {
     {title = "WELCOME", message = "Thanks for using XENO!"},
     {title = "HI", message = "hello - zxkuhl (note: this nodifications thingy is new!)"},
     {title = "DISCORD", message = "Join our community server for support"},
-    {title = "VALINTINES", message = "no update yet but happy valintines day!"},
     {title = "UP & COMING", message = "FORSAKEN SCRIPT! // COMING SOON"},
     {title = "NEW RELEASE", message = "GARDEN HORIZONS SCRIPT 🌱! // NEW, TRY IT OUT!"},
     {title = "SCRIPT UPDATE", message = "ABYSS SCRIPT UPDATE /// Maybe final update. improved features tho!"},
