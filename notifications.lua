ad458e2fe1a900b07111ce50b67e11e2051cd875
zxkuhl <zero98122@gmail.com>
2026-06-20 12:32:36 -0500

    Update notifications by removing old entries
    Removed outdated notifications and added a new welcome message.

--- notifications.lua
+++ notifications.lua
@@ -3,8 +3,5 @@ return {
     {title = "WELCOME", message = "Thanks for using XENO!"},
     {title = "HI", message = "hello - zxkuhl (note: this nodifications thingy is new!)"},
     {title = "DISCORD", message = "Join our community server for support"},
-    {title = "UP & COMING", message = "FORSAKEN SCRIPT! // COMING SOON"},
-    {title = "NEW RELEASE", message = "GARDEN HORIZONS SCRIPT 🌱! // NEW, TRY IT OUT!"},
-    {title = "SCRIPT UPDATE", message = "ABYSS SCRIPT UPDATE /// Maybe final update. improved features tho!"},
-    {title = "ZYNX", message = "ZYNX AI Made by zxkuhl is our help ai built to assist and guide you with your exploiting!"},
+    {title = "ZXKUHL", message = "im back :D - zxkuhl"},
 }
